799e4a744768adf06d7341b6d1f8f22d31fba900
Carlos Navarro Mallach <carlos.navarro.mallach@gmail.com>
2013-01-23 17:35:11 +0100

    Initial Commit

---
+++ Ejercicio1-T15/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ejercicio1-T15
+//
+//  Created by Carlos Navarro Mallach on 23/01/13.
+//  Copyright (c) 2013 Casa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ejercicio1-T15/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ejercicio1-T15
+//
+//  Created by Carlos Navarro Mallach on 23/01/13.
+//  Copyright (c) 2013 Casa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
