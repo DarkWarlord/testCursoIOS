a5b19b703a4d97463be960572481910d441930fe
Carlos Navarro Mallach <carlos.navarro.mallach@gmail.com>
2013-01-23 18:06:43 +0100

    Commit 2

--- Ejercicio1-T15/ViewController.m
+++ Ejercicio1-T15/ViewController.m
@@ -18,7 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"Hola mundo desde GIT");
+    NSLog(@"Hola mundo desde GIT - 2");
 }
 
 - (void)viewDidUnload
